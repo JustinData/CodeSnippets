e16d95691e66e9c920c7122360ffc62857501153
JustinGA <justinkemmerer@gmail.com>
2014-05-07 12:13:12 -0400

    Fixed the duplicateValueCheck.js snippet to actually work.

--- JavaScript/duplicateValueCheck.js
+++ JavaScript/duplicateValueCheck.js
@@ -3,19 +3,24 @@
 // To check a collection of objects for a duplicate value on a given key:
 //    Include the key as the second parameter , k.
 function duplicateValueCheck(c, k){
-	var check = false;
 	if (typeof(metricsData[0]) == "object"){
-		for (var i=0; i < c.length - 1; i++){
-			if (c[i + 1][k] == c[i][k]){
-				check = true;
+		for (var i = 0; i < c.length; i++) {
+			for (var j = 0; j < c.length; j++) {
+				if (i != j) { // Skips checking an index against itself
+					if (c[i][k] == c[j][k]) {
+					    return true; // Means there are duplicate values
+					}
+				}
 			}
 		}
 	} else {
+		var sortedArray = c.sort();
 		for (var i=0; i < c.length - 1; i++){
-			if (c[i + 1] == c[i]){
-				check = true;
+			if (sortedArray[i + 1] == sortedArray[i]){
+				return true;
 			}
 		}
 	}
-	return check;
+	return false;
 }
+
